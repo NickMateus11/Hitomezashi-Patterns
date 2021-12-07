9cd0aa324fb9b304254d3f5bdf942537e42c93a0
NickMateus11 <mateus.nicholas@gmail.com>
2021-12-07 17:10:43 -0500

    create function for pattern gen

--- src/hitomezashi_patterns.py
+++ src/hitomezashi_patterns.py
@@ -2,48 +2,53 @@ import cv2
 import numpy as np
 import random
 
-def main():
-    x_dim, y_dim = 800, 608
-    padding = 10
+padding = 10
 
-    x_cells = 16
-    y_cells = 16
+def hitomezashi(seq_x, seq_y, x_cells, y_cells, x_dim, y_dim):
+    canvas = np.zeros((y_dim+2*padding, x_dim+2*padding), dtype=np.uint8)
     
     x_step = (x_dim//x_cells)
     y_step = (y_dim//y_cells)
+    
+    for x in range(x_cells):
+        for y in range(y_cells//2):
+            bit = seq_x[x]
+            x_coord = x * x_step
+            y_coord = y*(2*y_step) + (bit*y_step)
+            cv2.line(canvas, 
+                    (x_coord+padding, y_coord+padding), 
+                    (x_coord+padding, y_coord+y_step+padding), 
+                    (255,)*3, 
+                    thickness=1
+                )
+    
+    for y in range(y_cells):
+        for x in range(x_cells//2):
+            bit = seq_y[y]
+            x_coord = x*(2*x_step) + (bit*x_step)
+            y_coord = y * y_step
+            cv2.line(canvas, 
+                    (x_coord+padding, y_coord+padding), 
+                    (x_coord+x_step+padding, y_coord+padding), 
+                    (255,)*3, 
+                    thickness=1
+                )
+
+    cv2.rectangle(canvas, (padding, padding), (x_dim+padding, y_dim+padding), (255,)*3, thickness=1)
+    
+    return canvas
+
+def main():
+    x_dim, y_dim = 800, 608
+
+    x_cells = 16
+    y_cells = 16
 
     while True:
-        canvas = np.zeros((y_dim+2*padding, x_dim+2*padding), dtype=np.uint8)
-        
         input_data_x = [int(random.random()*2) for _ in range(x_cells)]
         input_data_y = [int(random.random()*2) for _ in range(y_cells)]
         
-        for x in range(x_cells):
-            for y in range(y_cells//2):
-                bit = input_data_x[x]
-                x_coord = x * x_step
-                y_coord = y*(2*y_step) + (bit*y_step)
-                cv2.line(canvas, 
-                        (x_coord+padding, y_coord+padding), 
-                        (x_coord+padding, y_coord+y_step+padding), 
-                        (255,)*3, 
-                        thickness=1
-                    )
-        
-        for y in range(y_cells):
-            for x in range(x_cells//2):
-                bit = input_data_y[y]
-                x_coord = x*(2*x_step) + (bit*x_step)
-                y_coord = y * y_step
-                cv2.line(canvas, 
-                        (x_coord+padding, y_coord+padding), 
-                        (x_coord+x_step+padding, y_coord+padding), 
-                        (255,)*3, 
-                        thickness=1
-                    )
-
-        cv2.rectangle(canvas, (padding, padding), (x_dim+padding, y_dim+padding), (255,)*3, thickness=1)
-
+        canvas = hitomezashi(input_data_x, input_data_y, x_cells, y_cells, x_dim, y_dim)        
         cv2.imshow("win", canvas)
 
         key = cv2.waitKey(0) & 0xFF
